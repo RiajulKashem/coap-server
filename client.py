2278705ce1fab8f5b8a632c4cbe216ccca69bdc9
Riajul Kashem <riajul.programmer@gmail.com>
2022-07-06 18:33:17 +0600

    updated url

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ async def main():
     context = await Context.create_client_context()
     payload = b"sending from local client"
 
-    request = Message(code=POST, payload=payload, uri="coap://localhost/m")
+    request = Message(code=POST, payload=payload, uri="coap://3.74.88.33/m")
 
     response = await context.request(request).response
     print('Result: %s\n%r'%(response.code, response.payload))
